a9a6460ad6d3463b0703b66500f0fd7b2c3ccd23
tatina555 <katekinex100@gmail.com>
2020-10-27 14:41:53 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,4 @@
 class Room < ApplicationRecord
-  def change
-    create_table :rooms do |t|
-      t.string :name, null: false
-      t.timestamps
-    end
-  end
+  has_many :room_users
+  has_many :users, through: :room_users
 end
